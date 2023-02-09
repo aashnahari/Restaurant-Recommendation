554bfabaaa595f1f1db98248f4a9dbe170a01a53
Isha Krishnamurthy <isha@Ishas-MacBook-Pro-M1.local>
2023-02-09 09:44:37 -0500

    new index

--- db.js
+++ db.js
@@ -1,2 +1,40 @@
-app.get("/user/:id", db.getResturantByCuisine);
-app.get("/user/:id", db.getResturantByName);
+
+const getResturantByCuisine = (request, response) => {
+    const id = parseInt(request.params.id);
+    const query = `SELECT * FROM user WHERE id = ?`;
+    db.get(query, [id], (error, result) => {
+      if (error) {
+        console.error(error.message);
+        response.status(400).json({ error: error.message });
+        return;
+      }
+      // If nothing is returned, then result will be undefined
+      if (result) {
+        response.json(result);
+      } else {
+        response.sendStatus(404);
+      }
+    });
+  };
+
+
+
+  const getResturantByName = (request, response) => {
+    const id = parseInt(request.params.id);
+    const query = `SELECT * FROM user WHERE id = ?`;
+    db.get(query, [id], (error, result) => {
+      if (error) {
+        console.error(error.message);
+        response.status(400).json({ error: error.message });
+        return;
+      }
+      // If nothing is returned, then result will be undefined
+      if (result) {
+        response.json(result);
+      } else {
+        response.sendStatus(404);
+      }
+    });
+  };
+
+
