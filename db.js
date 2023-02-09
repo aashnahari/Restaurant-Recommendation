60bbd6a15dceb95e0e62f6f7b5f8ab6046a52946
Isha Krishnamurthy <isha@Ishas-MacBook-Pro-M1.local>
2023-02-09 09:46:11 -0500

    new db

--- db.js
+++ db.js
@@ -1,4 +1,10 @@
-
+const path = require("path");
+const sqlite = require("sqlite3").verbose();
+const dbFile = path.join(__dirname, "foo.db");
+const db = new sqlite.Database(dbFile, (error) => {
+  if (error) return console.error(error.message);
+  console.log(`Connected to database ${dbFile}`);
+});
 const getResturantByCuisine = (request, response) => {
     const id = parseInt(request.params.id);
     const query = `SELECT * FROM user WHERE id = ?`;
