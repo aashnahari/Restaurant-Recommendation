b1045c0fda677db135b8a3304e1fc2ef9239d79e
Aashna Hari <aashnahari@Aashnas-MacBook-Pro-2.local>
2023-04-20 14:25:20 -0400

    js

---
+++ front end/index.js
@@ -0,0 +1,7 @@
+streetVal = document.getElementById('streetInput').value
+avenueVal = document.getElementById('avenueInput').value
+zipcodeVal = document.getElementById('zipcodeInput').value
+
+console.log(streetVal);
+console.log(avenueVal);
+console.log(zipcodeVal);
